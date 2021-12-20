07c7ec24a1da48b6763ba53321450942ab20e393
AVOtekktion <91689292+AnriCzarnowski@users.noreply.github.com>
2021-12-20 18:34:47 +0100

    Create form3.cs

---
+++ form3.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Resourcepackv1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            this.BackgroundImage = Properties.Resources.jp2gigachad;
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+            Random random = new Random();
+            int x = random.Next(0, 1270);
+            int y = random.Next(0, 920);
+
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(x, y);
+            this.ShowInTaskbar = false;
+            InitializeComponent();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            List<Bitmap> lisimage = new List<Bitmap>();
+            lisimage.Add(Properties.Resources.jp2gigachad);
+            var indexbackimage = DateTime.Now.Second % lisimage.Count;
+            this.BackgroundImage = lisimage[indexbackimage];
+        }
+    }
+}
